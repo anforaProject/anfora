57440f673e07d17b1b1e51750d2a396fcb931dfa
Yabir <yabirgb@gmail.com>
2018-04-03 23:31:16 +0200

    Change jwt auth to token auth

--- src/storage.py
+++ src/storage.py
@@ -1,5 +1,7 @@
 import datetime
 import uuid
+import binascii
+import os
 
 from peewee import *
 from playhouse.shortcuts import model_to_dict
@@ -44,11 +46,26 @@ class Photo(BaseModel):
     def json(self):
         return model_to_dict(self, exclude=[User.password])
 
+
+class Token(BaseModel):
+
+    key = CharField(primary_key=True)
+    user = ForeignKeyField(User, on_delete='CASCADE')
+
+    def save(self, *args, **kwargs):
+        if not self.key:
+            self.key = self.generate_key()
+
+        return super(Token, self).save(*args, **kwargs)
+
+    def generate_key(self):
+        return binascii.hexlify(os.urandom(20)).decode()
+        
 def connect():
     db.connect()
     
 def create_tables():
-    tables = [User, Album, Photo]
+    tables = [User, Album, Photo, Token]
 
     for table in tables:
         if not table.table_exists():
