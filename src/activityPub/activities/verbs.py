e3b0245241edcef5a4217a14da0b42ce69933392
Yábir <yabirgb@gmail.com>
2018-04-18 22:45:46 +0200

    COde added during class

--- src/activityPub/activities/verbs.py
+++ src/activityPub/activities/verbs.py
@@ -4,4 +4,21 @@ from activityPub.activities.objects import (ALLOWED_TYPES, Object, Actor,
 from activityPub.activities import errors
 
 class Activity(Object):
-    pass
+    attributes = Object.attributes + ["actor", "object"]
+    type = "Activity"
+
+    def get_audience(self):
+
+        audience = []
+
+        for attr in ["to", "bto", "cc", "bcc", "audience"]:
+            value = getattr(self, attr, None)
+            if not value:
+                continue
+
+            if isinstance(value, str):
+                audience.append(value)
+
+        return set(audience)
+
+    def strip_audience(self):
