2d10a988400f0b03772766a5a162d842ed56e658
Yabir <yabirgb@gmail.com>
2018-04-21 16:50:38 +0200

    Added new code to manage inbox

---
+++ src/api/v1/activityPub/inbox.py
@@ -0,0 +1,40 @@
+import json
+import falcon
+
+from models.user import User
+from models.photo import Photo
+
+from activityPub import activities
+from activityPub.activities import as_activitystream
+
+from api.v1.activityPub.methods import (deliver, store, handle_note, handle_follow)
+
+class Inbox():
+
+    auth = {
+        'exempt_methods': ['GET']
+    }
+
+    def on_get(self, req, resp, username):
+
+        user = req.context['user']
+        objects = user.activities.select().where(remote==True).order_by(created_at.desc())
+        collection = activities.OrderedCollection(objects)
+
+        resp.body = collection.to_json(context=True)
+        resp.status = falcon.HTTP_200
+
+    def on_post(self, req, resp, username):
+
+        user = req.context['user']
+        payload = req.get_param('payload')
+        activity = json.loads(payload, object_hook=as_activitystream)
+        activity.validate()
+
+        if activity.type == "Create":
+            handle_note(activity)
+        elif activity.type == "Follow":
+            handle_follow(activity)
+
+        store(activity, user, remote = True)
+        resp.status= falcon.HTTP_200
