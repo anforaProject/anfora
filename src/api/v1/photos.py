1305cd5ce9eca825fe51c88e21cb20f2526f74c4
Yábir <yabirgb@gmail.com>
2018-06-05 14:23:44 +0200

    Started the refactoring of code

--- src/api/v1/photos.py
+++ src/api/v1/photos.py
@@ -7,7 +7,11 @@ import falcon
 from models.user import User
 from models.photo import Photo
 from models.album import Album
+
 from auth import (loadUser, auth_backend, try_logged_jwt)
+
+from tasks.tasks import create_image
+
 
 #Get max size for uploads
 MAX_SIZE = os.getenv('MAX_SIZE', 1024*1024)
@@ -79,25 +83,52 @@ class manageUserPhotos(object):
     def on_post(self, req, resp, user):
         image = req.get_param('image')
         public = bool(req.get_param('public')) #False if None
+        payload = req.get_param('data')
 
-        if image.filename:
-            user = req.context['user']
-            filename = image.filename
-            photo = Photo.create(title=filename,
-                                 public=public,
-                                 user=user,
-                                 media_type="image",
-                                 description=req.get_param('description'),
-                                 message=self._strip_message(req.get_param('message')))
-            print(photo, self.uploads)
-
+        if image != None and image.filename:
             try:
-                file_path = os.path.join(self.uploads, photo.media_name)
-                temp_file = file_path + '~'
-                open(temp_file, 'wb').write(image.file.read())
-                os.rename(temp_file, file_path)
-                resp.status = falcon.HTTP_201
-                resp.body = photo.json()
+
+                #Search a valid name
+
+                valid = False
+                ident = ""
+                while not valid:
+                    ident = str(uuid.uuid4())
+                    valid = not Photo.select().where(Photo.media_name == ident).exists()
+
+                #Pick the images paths
+                file_path = os.path.join(self.uploads, ident)
+
+                #temp_file = file_path + '~'
+                #open(temp_file, 'wb').write(image.file.read())
+
+                #Create the image and the thumbnail
+                create_image(io.BytesIO(image.file.read()), file_path)
+
+                user = req.context['user']
+                filename = image.filename
+                width, height = (1080,1080)
+
+                photo = Photo.create(title=filename,
+                                     user=user,
+                                     message = payload.message or '',
+                                     description = payload.description or '',
+                                     sensitive = payload.sensitive or '',
+                                     media_type="Image",
+                                     width = width,
+                                     height=height,
+                                     media_name=ident,
+                                     )
+
+            except IOError:
+                print(e)
+                photo.delete_instance()
+                resp.body = json.dumps({"Error": "Couldn't store file"})
+                resp.status = falcon.HTTP_500
+
+            activity.object.id = photo.uris.id
+            activity.id = store(activity, user)
+            resp.status = falcon.HTTP_200
 
             except Exception as e:
                 print(e)
