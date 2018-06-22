d43a3f9d76adcee9ba83a64519ff7e7459ee94a1
Yabir <yabirgb@gmail.com>
2018-06-22 16:30:24 +0200

    Refactor code

--- src/tasks/main.py
+++ src/tasks/main.py
@@ -1,8 +1,3 @@
 from tasks.config import huey  # import our "huey" object
-from tasks.tasks import count_beans  # import our task
-
-
-if __name__ == '__main__':
-    beans = input('How many beans? ')
-    count_beans(int(beans))
-    print('Enqueued job to count %s beans' % beans)
+from tasks.tasks import *
+from tasks.redis.spreadStatus import *
