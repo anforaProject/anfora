8cd9c23c68cc6abd83f631cf5f798b9cdc5910a9
Yabir <yabirgb@gmail.com>
2018-06-23 17:43:40 +0200

    Fixed some minor display errors with statuses

--- src/tasks/redis/spreadStatus.py
+++ src/tasks/redis/spreadStatus.py
@@ -9,6 +9,11 @@ from tasks.config import huey
 def spreadStatus(photo):
     r = redis.StrictRedis(host='localhost', port=6379)
     time = photo.created_at.timestamp()
+
+    #Populate all the followers timelines
     for follower in photo.user.followers():
         tagName = "{}:hometimeline".format(follower.username)
         r.zadd(tagName, time, photo.id)
+
+    #Add id to the own timeline
+    r.zadd("{}:hometimeline".format(photo.user.username), time, photo.id)
