790437f7f8307ea6c6840bf110cecacc4429613d
Yabir <yabirgb@gmail.com>
2018-07-01 02:58:18 +0200

    Reoganize settings file

--- src/settings.py
+++ src/settings.py
@@ -1,10 +1,15 @@
 
-protocol="http"
-
-NODENAME="aName"
-SUMMARY=""
-
+NODENAME = "aName"
+SUMMARY = ""
 SCHEME = "https"
-DOMAIN="zinat.test"
+DOMAIN ="zinat.test"
+
+MEADIA_FOLDER = "/home/yabir/killMe/uploads"
+
+#From here is not necessary to touch anything
 BASE_URL = "{}://{}".format(SCHEME, DOMAIN)
 ID=BASE_URL
+
+thumb_folder="small"
+pic_folder="max_resolution"
+icon="icons"
