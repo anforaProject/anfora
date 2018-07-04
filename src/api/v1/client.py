b8b92e838881366a7ae4b1439b33ff88a83a0dce
Yabir <yabirgb@gmail.com>
2018-07-04 11:44:26 +0200

    Add client under the same url as server

---
+++ src/api/v1/client.py
@@ -0,0 +1,14 @@
+import falcon
+
+from api.v1.helpers import load_template
+
+class VueClient(object):
+    auth = {
+        'exempt_methods': ['GET']
+    }
+    def on_get(self, req, resp, path):
+        html = load_template('index.html')
+
+        resp.status = falcon.HTTP_200
+        resp.content_type = 'text/html'
+        resp.body = html
