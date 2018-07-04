ca080c8d636f4795c676a83dca33bbb12d81a8f8
Yabir <yabirgb@gmail.com>
2018-07-04 21:59:44 +0200

    Added tests and travis config

--- src/utils/username.py
+++ src/utils/username.py
@@ -4,7 +4,14 @@ from models.user import User
 
 def extract_user(uri):
 
+    if uri.startswith("@"):
+        uri = uri[1:]
+    elif uri.startswith('acct:'):
+        uri = uri[5:]
+
     result = uri.split("@")
     if len(result) == 2:
         user, domain = result[0], result[1]
         return user, domain
+    else:
+        return None, None
