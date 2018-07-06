89cd6f04b70d6e0ae41236ad2798737710584c22
Yabir <yabirgb@gmail.com>
2018-07-06 23:41:57 +0200

    Ensure database has no open conection before opening one

--- src/middleware.py
+++ src/middleware.py
@@ -7,7 +7,8 @@ from models.base import db as database
 
 class PeeweeConnectionMiddleware(object):
     def process_request(self, req, resp):
-        database.connect()
+        if database.is_closed():
+            database.connect()
 
     def process_response(self, req, resp, resource):
         if not database.is_closed():
