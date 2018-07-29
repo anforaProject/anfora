43dd025e07928dbd55909cfe49cb9709016e7129
Yabir <yabirgb@gmail.com>
2018-07-29 22:39:40 +0200

    Added new settings format

---
+++ src/utils/settings_creator.py
@@ -0,0 +1,28 @@
+import yaml
+
+def create_settings(filen):
+    settings = yaml.load(open(filen))
+
+    template=(
+        f'NODENAME = "{settings.get("application_name")}"\n'
+        f'SUMMARY = "{settings.get("server_description")}"\n'
+        f'SCHEME = "{settings.get("scheme")}"\n'
+        f'DOMAIN ="{settings.get("domain")}"\n'
+        f'BASE_URL ="{settings.get("scheme")}://{settings.get("domain")}"\n'
+        'ID=BASE_URL\n'
+        f'MEDIA_FOLDER = "{settings.get("media_folder")}"\n'
+        f'thumb_folder="{settings.get("thumb_folder")}"\n'
+        f'pic_folder="{settings.get("image_folder")}"\n'
+        f'icon="{settings.get("icons_folder")}"\n'
+        f'salt_code="{settings.get("salt_code")}"'
+    )
+
+    return template 
+
+def create_settings_file(filen='config/docker.yaml', settings_path='settings.py'):
+
+    settings = create_settings(filen)
+    with open(settings_path, 'w') as f:
+        f.write(settings)
+
+#create_settings_file('/home/yabir/killMe/anfora/src/config/dev.yaml')
