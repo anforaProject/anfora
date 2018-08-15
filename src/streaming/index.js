e3d13b362a8a6a09446a05b097d8f8d65816a0cb
Yabir <yabirgb@gmail.com>
2018-08-15 09:15:36 +0200

    Use server side events

--- src/streaming/index.js
+++ src/streaming/index.js
@@ -1,7 +1,12 @@
-//Hapi stuff
-const Hapi = require('hapi')
-const Nes = require('nes')
-const AuthBearer = require('hapi-auth-bearer-token')
+var express = require('express');
+var app = express();
+var expressWs = require('express-ws')(app);
+var cors = require('cors')
+let sseExpress = require('sse-express');
+var passport = require('passport')
+var BearerStrategy = require('passport-http-bearer').Strategy;
+
+
 //DB
 const { Pool, Client } = require('pg')
 
@@ -13,67 +18,57 @@ const pool = new Pool({
     port: 5432,
   })
 
-const server = new Hapi.server({port: 4000})
-
 accountFromToken = (token) => {
     const text = "select * from token inner join userprofile on userprofile.id = token.user_id where token.key = $1"
     return pool.query(text, [token])
 }
 
-const start = async () => {
-    
-    await server.register([Nes, AuthBearer])
+passport.use(new BearerStrategy(
+    function(token, done) {
+        return accountFromToken(token)
+        .then(res => {
+            if (res.rows.length === 0){ 
+                return done(null, false); 
+            }
 
+            return done(null, res.rows[0], { scope: 'all' });
+        })
+    }
+));
 
-    server.auth.strategy('simple', 'bearer-access-token', {
-        allowQueryToken: true,              // optional, false by default
-        validate: async (request, token, h) => {
+app.use(cors())
+ 
+app.get('/',
+    passport.authenticate('bearer', { session: false }),
+    function(req, res, next){
+        console.log(req.user.username, req.testing);
+        res.end();
+    }
+);
 
-            // here is where you validate your token
-            // comparing with token from your database for example
-            return accountFromToken(token).then(
-                res=>{
-                    const isValid = res.rows.length !== 0;
-                    const credentials = { token };
-                    const artifacts = { user: res.rows[0] };
-                    return { isValid, credentials, artifacts };
-                }
-            )  
+app.get('/updates',passport.authenticate('bearer', { session: false }), sseExpress(),  function(req, res) {
+    res.sse({
+        event: 'connected',
+        data: {
+          welcomeMsg: 'Hello world!'
         }
     });
+    setInterval(function(){
+        let i = 0;
+        res.sse({
+            event: 'update',
+            data:{
+                int: i,
+                user: req.user.username
+            }
+        })  
+        i += 1;
+        console.log("pulse")
+    }, 3000)
+    
+});
 
-    server.route({
-        method: 'GET',
-        path: '/api/v1/streaming/user',
-        config: {
-            auth: 'simple',
-            id: 'homeTimeline'
-        },
-        handler: async function (request, h) {
-            return {info: 'success!'}
-        }
-    })
+ 
+app.listen(4000);
 
-    await server.start();
-
-    return server;
-}
-
-/*
-accountFromToken("155d65450328a7fbf4915bb0a829ee7cadf4a631").then(res=>{
-    console.log(res.rows)
-})
-*/
-
-accountFromToken("155d65450328a7fbf4915bb0a829ee7cadf4a631").then(res=>{
-    console.log(res.rows[0])
-})
-
-start()
-.then((server) => console.log(`Server listening on ${server.info.uri}`))
-.catch(err => {
-
-    console.error(err);
-    process.exit(1);
-})
 
