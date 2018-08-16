9618bdb3fc9df6b5e11b52267c40fc68bf0cf20c
Yabir <yabirgb@gmail.com>
2018-08-16 23:48:06 +0200

    Timelines updated

--- src/tasks/redis/spreadStatus.py
+++ src/tasks/redis/spreadStatus.py
@@ -6,6 +6,8 @@ from models.user import UserProfile
 
 from tasks.config import huey
 
+from utils.timeline_manager import TimelineManager
+
 @huey.task()
 def spread_status(status):
     r = redis.StrictRedis(host=os.environ.get('REDIS_HOST', 'localhost'))
@@ -13,9 +15,8 @@ def spread_status(status):
 
     #Populate all the followers timelines
     for follower in status.user.followers():
-        tagName = "feed:home:{}".format(follower.id)
-        r.zadd(tagName, status.id, time)
+        TimelineManager(follower).push_home(status)
         r.publish(f'timeline:{follower.id}', f'CREATE {status.id}')
 
     #Add id to the own timeline
-    r.zadd("feed:home:{}".format(status.user.id), time, status.id)
+    TimelineManager(status.user).push_home(status)
