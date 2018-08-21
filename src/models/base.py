c53264af75a53c4e0bc2d89a255dc1d65b40fa9a
Yabir <yabirgb@gmail.com>
2018-08-21 18:38:23 +0200

    Update to manage postgresql config

--- src/models/base.py
+++ src/models/base.py
@@ -1,12 +1,13 @@
 from peewee import *
+from settings import (DB_USER, DB_NAME, DB_PORT, DB_HOST)
 from playhouse.postgres_ext import PostgresqlExtDatabase
 
 #db = SqliteDatabase('zinat.db')
 db = PostgresqlExtDatabase(
-    'anforadb',  # Required by Peewee.
-    user='postgres',  # Will be passed directly to psycopg2.
+    DB_NAME,  # Required by Peewee.
+    user=DB_USER,  # Will be passed directly to psycopg2.
     host='localhost',  # Ditto.
-    port='5432',
+    port=DB_PORT,
 )
 
 class BaseModel(Model):
