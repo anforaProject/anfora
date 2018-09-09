02eeef788980c2e77689a56bb876cd2248b3063d
Yabir <yabirgb@gmail.com>
2018-09-10 00:11:51 +0200

    Publish notificaitons on redis

--- src/tasks/redis/spreadStatus.py
+++ src/tasks/redis/spreadStatus.py
@@ -4,10 +4,11 @@ import json
 
 from models.status import Status
 from models.user import UserProfile
+from models.notification import Notification, notification_types
 
 from tasks.config import huey
 
-from utils.timeline_manager import TimelineManager
+from managers.timeline_manager import TimelineManager
 
 @huey.task()
 def spread_status(status):
@@ -24,3 +25,27 @@ def spread_status(status):
     #Add id to the own timeline
     TimelineManager(status.user).push_home(status)
     r.publish(f'timeline:{status.user.id}', f'update {json_file}')
+
+@huey.task()
+def like_status(status, user):
+
+    """
+        status - the status target of the like - Status
+        user - the user liking the post - UserProfile
+    """
+
+    r = redis.StrictRedis(host=os.environ.get('REDIS_HOST', 'localhost'))
+    
+    #Add id to the like timeline
+    TimelineManager(status.user).push_likes(status)
+    
+    notification = Notification.create(
+        user = user,
+        target = status.user,
+        status = status,
+        notification_type = notification_types['like']
+    )
+
+    json_file = json.dumps(status.json(), default=str)
+
+    r.publish(f'timeline:{status.user.id}', f'notification {json_file}')
