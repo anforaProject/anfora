d3f3e4eb8a6d98dc05e6b332f0d5bdee61238bfd
Yábir García <yabirgb@gmail.com>
2018-09-30 21:06:59 +0200

    Separate actor from user in api

--- src/api/v1/activityPub/actor.py
+++ src/api/v1/activityPub/actor.py
@@ -1,3 +1,7 @@
+import json 
+import falcon
+from models.user import User, UserProfile
+
 class getActor():
     auth = {
         'exempt_methods': ['GET']
@@ -7,7 +11,7 @@ class getActor():
         person = User.get_or_none(username=username)
         if person:
             person = person.profile.get()
-            resp.body = json.dumps(person.to_json(), default=json_serial)
+            resp.body = json.dumps(person.to_activitystream(), default=str)
             resp.status = falcon.HTTP_200
         else:
             resp.status = falcon.HTTP_404
