b51a9ec6ecdeb83f2d423f3730f26c64ad5d29b8
Yábir García <yabirgb@gmail.com>
2018-10-11 14:47:35 +0200

    Update paths and added support for aioredis

--- src/api/v1/streaming.py
+++ src/api/v1/streaming.py
@@ -6,36 +6,36 @@ from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop, PeriodicCallback
 from tornado.iostream import StreamClosedError
 
-from base_handler import BaseHandler
-from auth.token_auth import bearerAuth
+from auth_decorators.token_auth import bearerAuth
 
-from aioredis.pubsub import Receiver
+from api.v1.base_handler import BaseHandler
+
+import aioredis
 
 class Notification(BaseHandler):
 
-    self.prefix = 'timeline'
+    async def handle_msg(msg):
+        event, data = msg.split(" ", 1)
+        self.write('event: {}\n data: {}\n\n'.format(event, data))
+        self.flush()
 
-    def initialize(self, source):
-        """The ``source`` parameter is a string that is updated with
-        new data. The :class:`EventSouce` instance will continuously
-        check if it is updated and publish to clients when it is.
-        """
-        self.source = source
-        self._last = None
-        self.set_header('content-type', 'text/event-stream')
-        self.set_header('cache-control', 'no-cache')
-
-    async def reader(mpsc):
-        async for channel, msg in mpsc.iter():
-            assert isinstance(channel, AbcChannel)
-                print("Got {!r} in channel {!r}".format(msg, channel))
+    async def reader(ch):
+        while (await ch.wait_message()):
+            msg = await ch.get_json()
+            asyncio.ensure_future(handle_msg(msg))
 
     @bearerAuth
     async def get(self, user):
-        
+        self.set_header('content-type', 'text/event-stream')
+        self.set_header('cache-control', 'no-cache')
         mpsc = Receiver(loop=loop)
         asyncio.ensure_future(self.reader(mpsc))
+        prefix = "timeline"
+        channel = f'{prefix}:{user.id}'
 
-        channel = f'{self.prefix}:{user.id}'
+        sub = await aioredis.create_redis(('localhost', 6379))
+        res = await sub.subscribe(channel)
+        ch1 = res[0]
+        tsk = await reader(ch1)
 
-        await redis.subscribe(mpsc.channel('channel:1'),
+        #831d3b4b6dfac6ff0443b18a805415af845f47ed
--- src/tornado_server.py
+++ src/tornado_server.py
@@ -1,6 +1,4 @@
-import tornado.ioloop
-import tornado.web
-from tornado.platform.asyncio import AsyncIOMainLoop
+import tornado
 import asyncio
 
 import peewee_async
@@ -18,7 +16,7 @@ if __name__ == "__main__":
     #AsyncIOMainLoop().install()
     app = make_app()
 
-    app.listen(8888)
+    app.listen(4000)
     app.objects = peewee_async.Manager(db)
     #loop = asyncio.get_event_loop().run_forever()
     tornado.ioloop.IOLoop.current().start()
