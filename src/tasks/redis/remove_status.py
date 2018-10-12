5eaf59395b78484898d9978eb5054d032c6e2a3f
Yábir García <yabirgb@gmail.com>
2018-10-12 23:56:52 +0200

    Fix error removing all the posts

--- src/tasks/redis/remove_status.py
+++ src/tasks/redis/remove_status.py
@@ -15,14 +15,14 @@ def remove_status(status):
     r = redis.StrictRedis(host=os.environ.get('REDIS_HOST', 'localhost'))
 
     # Remove it from the own timeline
-    TimelineManager(status.user).remove_from_home(status.id)
+    TimelineManager(status.user).remove_from_home(status)
     # Remove it from the followers timeline
     for follower in status.user.followers():
-        TimelineManager(follower).remove_from_home(status.id)
+        TimelineManager(follower).remove_from_home(status)
     
     #Update the user posts count
 
     UserProfile.update({UserProfile.statuses_count: UserProfile.statuses_count - 1}).where(UserProfile.id == status.user.id).execute()
 
     # Remove the status
-    status.delete().execute()
+    status.delete_instance().execute()
