d17d322016cbd511884d8cca3db63db3c1c7e790
Yábir García <yabirgb@gmail.com>
2018-11-10 19:50:42 +0100

    Add new pagination

---
+++ src/decorators/pagination.py
@@ -0,0 +1,18 @@
+import falcon
+from settings import DOMAIN, SCHEME
+
+def ap_pagination(obj):
+    pagination = {
+        'default_pagination': 10,
+        'id': f'{SCHEME}://{DOMAIN}{obj.request.path}',
+        'partOf': f'{SCHEME}://{DOMAIN}{obj.request.path}',
+    }
+
+    if obj.request.query_arguments:
+        pagination['id'] += f"?{''.join([f'{x}={str(obj.request.query_arguments[x][0].decode())}' for x in obj.request.query_arguments])}"
+     
+    
+    pagination['page'] = int(obj.get_argument('page', 0))
+    pagination['next'] = f'{SCHEME}://{DOMAIN}{obj.request.path}?page={pagination["page"]+1}'
+    return pagination
+    
