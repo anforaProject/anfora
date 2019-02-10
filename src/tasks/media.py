72c4c3b10ec389d8af798e8f80eb954f0b6f7a8e
Yábir García <yabirgb@gmail.com>
2019-02-10 01:21:21 +0100

    Move the atachment to a task

--- src/tasks/media.py
+++ src/tasks/media.py
@@ -1,6 +1,9 @@
-import redis
 import os
 import json
+import logging
+from typing import Tuple
+
+import redis
 
 from models.status import Status
 from models.media import Media
@@ -10,8 +13,11 @@ from models.notification import Notification, notification_types
 from tasks.config import huey
 from managers.media_manager import MediaManager
 
+logger = logging.getLogger(__name__)
+
+
 @huey.task()
-def store_media(manager, ident, description, focus):
+def store_media(manager: MediaManager, ident: str, description: str, focus:Tuple[int, int]) -> None:
 
     #Send task to create image object
     width, height, mtype = manager.store_media(ident)
@@ -22,7 +28,7 @@ def store_media(manager, ident, description, focus):
         focus_y = 0
 
         if focus:
-            focus_x, focus_y = focus.split(',')
+            focus_x, focus_y = focus
 
         m = Media.create(
             media_name = ident,
@@ -33,3 +39,12 @@ def store_media(manager, ident, description, focus):
             media_type = mtype,
             description = description,
         )
+
+@huey.task()
+def atach_media_to_status(status, media_id):
+    try:
+        m = Media.get(Media.media_name==media_id)
+        m.status = status
+        m.save()
+    except Media.DoesNotExist:
+        logger.error(f"Media id not found {image} for {status.id}")
