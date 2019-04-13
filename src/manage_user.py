196cfbb28f11329fcae75c4133cd2a7409f16a09
yalh76 <yalh@yahoo.com>
2019-04-13 16:42:24 +0200

    indentation

--- src/manage_user.py
+++ src/manage_user.py
@@ -7,13 +7,13 @@ from settings import salt_code
 
 def create_user(username='config/docker.yaml', password='settings.py', email='settings.py', is_admin='settings.py'):
     
-new_user(
-        username = username, 
-        password = password, 
-        email = email,
-        description= "",
-        confirmed=True,
-        is_admin = admin,
-        is_remote=False,
-        is_private=False
-    )
+    new_user(
+            username = username, 
+            password = password, 
+            email = email,
+            description= "",
+            confirmed=True,
+            is_admin = admin,
+            is_remote=False,
+            is_private=False
+        )
